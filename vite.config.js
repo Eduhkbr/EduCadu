f97471a175a03818a011e2c54c1a6cec73900e3c
Eduardo Abreu <edu.abreu09@gmail.com>
2025-07-03 21:15:18 -0300

    Feat: App install

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Adicione o plugin aqui
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'EduCadu',
+        short_name: 'EduCadu',
+        description: 'Um aplicativo divertido e seguro para seu filho aprender.',
+        theme_color: '#E0F2FE',
+        background_color: '#E0F2FE',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'icon-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
 })
